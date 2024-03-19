ed34f93061630016aad9bcd18ec6d349cc3ea500
Nelson Mecha <nelsonorinamecha@gmail.com>
2024-03-19 11:28:27 +0300

    arithmetic lab

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
+const num1 = 2;
+const num2 = 31;
+let multiply = num1 * num2;
 
+const num3 = 24;
+const num4 = 10;
+const mod = num3 % num4;
+console.log(mod)
+
+let random = 0;
+++random
+console.log(random)
+let max=20
+Math.max(2, 4, 6, 8, 10, 12, 14, 16, 18, 20)
+console.log(Math.max)
 // Write your code here
